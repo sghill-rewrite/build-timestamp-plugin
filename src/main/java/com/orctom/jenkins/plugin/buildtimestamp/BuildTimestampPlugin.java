5b8641a0f4b28e86556c1ae2fb76646acfe23f95
Hao <orctom@gmail.com>
2015-12-20 02:04:27 +0800

    changed defualt format

--- src/main/java/com/orctom/jenkins/plugin/buildtimestamp/BuildTimestampPlugin.java
+++ src/main/java/com/orctom/jenkins/plugin/buildtimestamp/BuildTimestampPlugin.java
@@ -20,10 +20,9 @@ import java.util.*;
  */
 public class BuildTimestampPlugin {
 
-
 	public static final String PLUGIN_NAME = "Build Timestamp Plugin";
 
 	public static final String DEFAULT_PROPERTY = "BUILD_TIMESTAMP";
-	public static final String DEFAULT_PATTERN = "yyyy-MM-dd HH:mm:ss ZZZ";
+	public static final String DEFAULT_PATTERN = "yyyy-MM-dd HH:mm:ss z";
 
 }
